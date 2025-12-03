f51d9ad42fc10ba3c2b5ebfc3ed408206c50f1ad
CiciLiu1618 <CiciLiu1618@gmail.com>
2025-12-02 20:31:44 -0600

    assets added

--- Assets/Henry/NPCInteract.cs
+++ Assets/Henry/NPCInteract.cs
@@ -5,34 +5,46 @@ public class NPCInteract : MonoBehaviour
 {
     public GameObject PressE;
     public GameObject Message;
-
+    
     public bool inTrigger;
 
     private void Start()
-    {
-        inTrigger = false;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        PressE.SetActive(true);
-    }
-
-    private void OnTriggerExit(Collider other)
     {
         inTrigger = false;
         PressE.SetActive(false);
         Message.SetActive(false);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inTrigger = true;
+            PressE.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inTrigger = false;
+            PressE.SetActive(false);
+            Message.SetActive(false);
+        }
+    }
+
     private void OnTriggerStay(Collider other)
     {
-        inTrigger = true;
+        if (other.CompareTag("Player"))
+        {
+            inTrigger = true;
+        }
     }
 
     public void OnPressE(InputAction.CallbackContext context)
     {
-        if(inTrigger == true)
+        if (context.performed && inTrigger)
         {
             Message.SetActive(true);
             PressE.SetActive(false);
